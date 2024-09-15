0c84156e2bdfc51a7e1c884eadda16f3ec1827c3
Altaf Yafai <49188943+AltafYafai@users.noreply.github.com>
2024-09-16 00:02:45 +0530

    Update device.mk
    removed hardware 2.1 so

--- device.mk
+++ device.mk
@@ -495,7 +495,6 @@ PRODUCT_COPY_FILES += \
 PRODUCT_PACKAGES += \
     android.frameworks.sensorservice@1.0 \
     android.frameworks.sensorservice@1.0.vendor \
-    android.hardware.sensors@2.1-service.xiaomi-multihal \
     libsensorndkbridge
 
 PRODUCT_COPY_FILES += \

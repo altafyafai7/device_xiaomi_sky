35b8ea9fd0787d54baacd6c0f27c407b50ac7e45
Altaf Yafai <49188943+AltafYafai@users.noreply.github.com>
2024-09-16 00:58:57 +0530

    Update and rename aosp_sky.mk to derp_sky.mk
    Bringup for derp unofficial

--- derp_sky.mk
+++ derp_sky.mk
@@ -12,8 +12,8 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
 # Configure full_base_telephony.mk
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 
-# Inherit common PixelOS configurations
-$(call inherit-product, vendor/aosp/config/common_full_phone.mk)
+# Inherit common Derpfest configurations
+$(call inherit-product, vendor/derp/config/common_full_phone.mk)
 
 # Inherit device configurations
 $(call inherit-product, $(DEVICE_PATH)/device.mk)
@@ -21,12 +21,18 @@ $(call inherit-product, $(DEVICE_PATH)/device.mk)
 # Inherit from the proprietary version
 $(call inherit-product, vendor/xiaomi/sky/sky-vendor.mk)
 
+# Derpfest
+DERP_BUILDTYPE := UnOfficial
+
 ## Device identifier
 PRODUCT_DEVICE := sky
-PRODUCT_NAME := aosp_sky
+PRODUCT_NAME := derp_sky
 PRODUCT_BRAND := Redmi
 PRODUCT_MODEL := 23076RN4BI
 PRODUCT_MANUFACTURER := Xiaomi
+
+#GApps
+TARGET_USES_PICO_GAPPS := true
 
 # GMS
 PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
